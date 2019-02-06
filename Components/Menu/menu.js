6bcf57ebd41de516874b69e87ffd7df767865405
Will Schulz <will.schulz29@gmail.com>
2019-02-06 10:31:17 -0400

    react login works

--- Components/Menu/menu.js
+++ Components/Menu/menu.js
@@ -2,6 +2,6 @@ const toggleMenu = () => {
     menu.classList.toggle('menu--open');
   }
   
-  const menu = document.querySelector('.menu');
-  const menuButton = document.querySelector('.menu-button');
-  menuButton.addEventListener('click', () => toggleMenu());
+const menu = document.querySelector('.menu');
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', () => toggleMenu());
