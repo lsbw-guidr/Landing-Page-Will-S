07193fc7d719d7b3fb79d75b6ee551ac84cf7690
Will Schulz <will.schulz29@gmail.com>
2019-02-06 21:43:11 -0400

    still styling

---
+++ Images/Landing Sign-Up_files/BizibleAcct.js
@@ -0,0 +1,12 @@
+(function () {
+    BizTrackingA.GoAccount({
+        webToLeadField: '',
+        chatEnabled: 'true' === 'true',
+        XUserId: 'c4a6a4a4925845da8638db710a164d42',
+        formProviderEnabled: 'true' === 'true',
+        attach_secure_forms: 'true' === 'true',
+        viewThroughEnabled: 'true' === 'true',
+        interceptAjax: 'true' === 'true'
+    });
+})();
+;
